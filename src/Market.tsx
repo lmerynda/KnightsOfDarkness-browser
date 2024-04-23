9955b0da0feaba19a357b6c81044fe02a04102dd
emerluk <lmerynda@gmail.com>
2024-04-23 01:10:06 +0000

    basic market view

--- src/Market.tsx
+++ src/Market.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
 
+type MarketData = {
+    id: string;
+    kingdom: string;
+    resource: string;
+    price: number;
+    count: number;
+};
+
 const Market: React.FC = () => {
+    // react state to hold market data from the server
+    const [marketData, setMarketData] = React.useState<MarketData[]>();
+
     React.useEffect(() => {
         fetch('http://localhost:8080/market')
             .then(response => response.json())
-            .then(data => console.log(JSON.stringify(data)));
+            .then(data => {
+                setMarketData(data);
+            });
     }, []);
+
+    React.useEffect(() => {
+        if (marketData) {
+            console.log(`Market data: ${JSON.stringify(marketData)}`);
+        }
+    }, [marketData]);
 
     return (
         <div>
             <h1>Market</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Kingdom</th>
+                        <th>Resource</th>
+                        <th>Price</th>
+                        <th>Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {marketData?.map((data) => (
+                        <tr key={data.id}>
+                            <td>{data.kingdom}</td>
+                            <td>{data.resource}</td>
+                            <td>{data.price}</td>
+                            <td>{data.count}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
